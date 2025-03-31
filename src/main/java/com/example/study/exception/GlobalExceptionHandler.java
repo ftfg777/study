07cedbfa82c47e6af1db749c9b05d3bcb207ce35
Jeong Chan Woo <4245093@naver.com>
2025-03-31 20:31:41 +0900

    apiresponse 수정

--- src/main/java/com/example/study/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/study/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.example.study.exception;
 
 import com.example.study.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,13 +10,17 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(CommonExceptionHandler.class)
     public ResponseEntity<ApiResponse<Void>> handleUserCommonException(CommonExceptionHandler ex){
-        return ResponseEntity.ok(ApiResponse.error(ex.getErrorCode()));
+        return ResponseEntity
+            .status(ex.getErrorCode().getStatus())
+            .body(ApiResponse.error(ex.getErrorCode()));
     }
 
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleException() {
-        return ResponseEntity.ok(ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR));
+    public ResponseEntity<ApiResponse<Void>> handleException(Exception ex) {
+        return ResponseEntity
+            .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus())
+            .body(ApiResponse.error(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 
 }
